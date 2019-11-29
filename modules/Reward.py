1834326264490c5e713d531148e54204fe7c627d
ScientiaEtVeritas <f@retkow.ski>
2019-11-29 19:50:03 +0100

    ROUGE reward

--- modules/Reward.py
+++ modules/Reward.py
@@ -1,5 +1,15 @@
 from modules.bleu import compute_bleu
+import rouge
 import torch
+
+compute_rouge = rouge.Rouge(metrics=['rouge-w'],
+                       limit_length=True,
+                       length_limit=100,
+                       length_limit_type='words',
+                       apply_avg=True,
+                       alpha=0.5, # Default F1_score
+                       weight_factor=1.2,
+                       stemming=False)
 
 class Reward(object):
     def __init__(self, config):
@@ -18,6 +28,16 @@ class Reward(object):
                     tgt_ = tgt_[:-1] if tgt_[-1] == self.config.tgt_eos else tgt_
                     bleu_score = compute_bleu([[tgt_]], [otp], max_order=4, smooth = True)[0]
                     rewards[dj][ri] = bleu_score
+            elif reward == "ROUGE":
+                for dj in range(self.config.BATCH_SIZE):
+                    otp = output[dj].tolist()
+                    otp = otp[:-1] if otp[-1] == self.config.tgt_eos else otp
+                    otp = " ".join(str(x) for x in otp) 
+                    tgt_ = tgt[dj][1:].squeeze().tolist()
+                    tgt_ = tgt_[:-1] if tgt_[-1] == self.config.tgt_eos else tgt_
+                    tgt_ = " ".join(str(x) for x in tgt_)
+                    rouge_score = compute_rouge.get_scores([otp], [[tgt_]])['rouge-w']['f']
+                    rewards[dj][ri] = rouge_score
         
         # Weighting rewards
         rewards = (rewards * torch.Tensor(self.config.rewards_weights)).sum(dim=1)        
